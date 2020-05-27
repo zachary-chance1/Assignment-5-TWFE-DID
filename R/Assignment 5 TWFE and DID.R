6af35f8288c17c6b6c736fecf53e14a5a4f49b22
zachary-chance1 <Zachary_Chance1@baylor.edu>
2020-05-27 18:58:34 -0500

    Added subdirectories and the R script for Assignment 5 to the repo

---
+++ R/Assignment 5 TWFE and DID.R
@@ -0,0 +1,7 @@
+################################################################
+#name: Assignment 5 TWFE and DID
+#author: zachary chance (baylor university)
+#description: completes assignment 5 and performs analysis
+#             on the assigned data under TWFE and DID scenarios
+#date: may 27, 2020
+#################################################################
