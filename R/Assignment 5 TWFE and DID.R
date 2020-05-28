ed2c495a41cca476530c0aa1c5c12e5b96c9d757
zachary-chance1 <Zachary_Chance1@baylor.edu>
2020-05-27 19:12:13 -0500

    Read in the data from the data folder and also upload the data as a csv

--- R/Assignment 5 TWFE and DID.R
+++ R/Assignment 5 TWFE and DID.R
@@ -5,3 +5,16 @@
 #             on the assigned data under TWFE and DID scenarios
 #date: may 27, 2020
 #################################################################
+
+
+WORK.DIR = "C:/Users/Owner/Desktop/Assignment-5-TWFE-DID"
+
+# set working directory
+setwd(WORK.DIR)
+
+library(AER)
+library(haven)
+library(tidyverse)
+
+
+problem_1_data = read.csv("Data/hw5_ex1.csv")
